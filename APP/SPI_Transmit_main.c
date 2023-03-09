577941d635e58bb692c09da0097b3839544fbd20
Adham AbdulAziz <93765430+Adhamsalah22@users.noreply.github.com>
2023-03-09 08:47:07 +0200

    Add files via upload

---
+++ APP/SPI_Transmit_main.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include "../HAL/LED/led.h"
+#include "../MCAL/SPI/SPI.h"
+#include "../HAL/LED/led.h"s
+
+/*
+int main(void) {
+	
+	SPI_VoidInit();
+	led_init();
+	while (1) {
+		char RX_Result = SPI_u8SendRecData(4,3);
+
+		if (RX_Result ==4)
+		{
+			led_on(LED_BLUE);
+		}
+		if (RX_Result == 3)
+		{
+			led_on(LED_BLUE);
+		}
+
+
+	}
+
+	return 0;
+}
+*/
---
+++ APP/UART_main.c
@@ -0,0 +1,57 @@
+/*
+ * UART.c
+ *
+ * Created: 3/8/2023 10:34:05 AM
+ * Author : ADHAM ABDELAZIZ
+ */ 
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include "../MCAL/DIO/dio.h"
+#include "../MCAL/UART/uart.h"
+#include "../HAL/LED/led.h"
+#include <avr/io.h>
+
+/****************************************************************/
+/* Description   : Testing Normal Mode                          */
+/****************************************************************/
+// int main(void)
+// {
+//     char c;
+//     UART_Init(UART_BAUDRATE_9600);
+//     led_init();
+//     UART_TransmitStr("Adham Salah");
+// 	
+//     while(1)
+//     {
+// 		c = UART_ReceiveChr();
+// 		UART_TransmitChr(c);
+// 		led_on(LED_GREEN);
+// 
+// 	
+// 			
+//     }
+// }
+
+/****************************************************************/
+/* Description   : Testing Interrupt Mode                       */
+/****************************************************************/
+
+/*
+int main(void)
+{
+	led_init();
+	UART_Init(UART_BAUDRATE_9600);
+	GIE_voidEnable();
+	UART_TransmitStr("Adham Salah Mahmoud Abdelaziz");
+
+	while(1)
+	{
+		UART_TransmitStr("Adham Salah Mahmoud Abdelaziz");
+
+		led_on(LED_GREEN);
+		_delay_ms(100);
+		led_off(LED_GREEN);
+		_delay_ms(100);
+	}
+}
+*/
