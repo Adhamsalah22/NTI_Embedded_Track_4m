149574ede448c94dcaafdf6311cae1ebbe97920e
Adham AbdulAziz <93765430+Adhamsalah22@users.noreply.github.com>
2023-03-11 09:43:48 +0200

    Add files via upload

---
+++ APP/ICU HW/ICU HW/main.c
@@ -0,0 +1,18 @@
+/*
+ * ICU HW.c
+ *
+ * Created: 3/11/2023 9:35:15 AM
+ * Author : ADHAM ABDELAZIZ
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
